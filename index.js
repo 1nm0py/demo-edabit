82dbb4a35e845a4e542a69aa449b43f191b7c86e
Inamov Asilbek <playerallgames007@gmail.cpm>
2024-03-24 05:33:06 +0500

    Fix01

---
+++ index.js
@@ -0,0 +1,386 @@
+// # demo-edabit
+
+// [1. How Edabit Works](https://edabit.com/challenge/ARr5tA458o2tC9FTN)
+// ```js
+// function hello() {
+//     return "hello edabit.com";
+// }
+// ```
+// [2. Return the Sum of Two Numbers](https://edabit.com/challenge/3LpBLgNRyaHMvNb4j)
+// ```js
+// function addition(a, b) {
+//     return a + b
+// }
+// ```
+// [3. Convert Minutes into Seconds](https://edabit.com/challenge/8q54MKnRrm89pSLmW)
+// ```js
+
+// function convert(minutes) {
+//     return minutes * 60;
+// }
+// ```
+// [4. Return the Next Number from the Integer Passed](https://edabit.com/challenge/NAQhEoxbofPidLxm9)
+// ```js
+// function addition(num) {
+//     return ++num
+// }
+// ```
+// [5. Area of a Triangle](https://edabit.com/challenge/3CaszbdZYGN4otQD8)
+// ```js
+// function triArea(base, height) {
+// 	return (base * height) / 2
+// }
+// ```
+// [6. Convert Age to Days](https://edabit.com/challenge/bL7hSc6Zh4zZJzGmw)
+// ```js
+// function calcAge(age) {
+// 	return age * 365
+// }
+// ```
+// [7. Buggy Code (Part 1)](https://edabit.com/challenge/j7yQbF3J3rToHsDBP)
+// ```js
+// function cubes(a) {
+// 	return a ** 3
+// }
+// ```
+// [8. Return the First Element in an Array](https://edabit.com/challenge/QaApgtePE6QrCZ64o)
+// ```js
+// function getFirstValue(arr) {
+// 	return arr[0];
+// }
+// ```
+// [9. Power Calculator](https://edabit.com/challenge/wAdE9te55cowBLcPs)
+// ```js
+// function circuitPower(voltage, current) {
+// 	return voltage * current;
+// }
+// ```
+// [10. Convert Hours into Seconds](https://edabit.com/challenge/6AnQqiEjkJdZrWhPS)
+// ```js
+// function howManySeconds(hours) {
+// 	return hours * 3600;
+// }
+// ```
+// [11. Maximum Edge of a Triangle]( https://edabit.com/challenge/nhXofMMyrowMyr9Nv )
+// ```js
+// function nextEdge(side1, side2) {
+// 	return side1 + side2 - 1;
+// }
+// ```
+// [12. Return the Remainder from Two Numbers]( https://edabit.com/challenge/Q2j5FTFtsk7PdzrQk )
+// ```js
+// function remainder(x, y) {
+// 	return x % y;
+// }
+// ```
+// [13. Find the Perimeter of a Rectangle]( https://edabit.com/challenge/XnJ24rWW7iJkNrtsh )
+// ```js
+// function findPerimeter(length,width){
+// 	return (length + width) * 2;
+// }
+// ```
+// [14. Return Something to Me!]( https://edabit.com/challenge/MvZK536X7fyrWH8Qc )
+// ```js
+// function giveMeSomething(a) {
+// 	return "somethink " + a
+// }
+// ```
+// [15. Correct the Mistakes]( https://edabit.com/challenge/ks3vMrqdnW3CQ3F4i )
+// ```js
+// function squared(b) {
+// 	return b*b;
+// }
+// ```
+// [16. Is the Number Less than or Equal to Zero?]( https://edabit.com/challenge/PTiLYyb4A69KZtBCg )
+// ```js
+// function lessThanOrEqualToZero(num) {
+// 	if(num <= 0)
+// 		return true;
+// 	else
+// 		return false;
+// }
+// ```
+// [17. Sum of Polygon Angles]( https://edabit.com/challenge/fBJyQSe5Jmbm9hPAG )
+// ```js
+// function sumPolygon(n) {
+// 	return (n - 2) x 180
+// }
+// ```
+// [18. Basketball Points]( https://edabit.com/challenge/Y46Xp2pcvTB77bmdD )
+// ```js
+// function points(twoPointers, threePointers) {
+// 	return twoPointers*2+threePointers*3
+// }
+// ```
+// [19. Basic Variable Assignment]( https://edabit.com/challenge/ZNwHGgHvsdnYwJ5WK )
+// ```js	
+// function nameString(name){
+// 	return name + "Edabit"
+// }
+// ```
+// [20. Less Than 100?]( https://edabit.com/challenge/9MjEpkL7yAjAqiH58 )
+// ```js
+// function lessThan100(a, b) {
+// 	if (a + b < 100){
+// 		return true
+// 	}
+// 	else {
+// 		return false
+// 	}
+// }
+// ```
+// [21. Buggy Code (Part 5)]( https://edabit.com/challenge/4iCsexZgmDEMMxj46 )
+// ```js
+// function printArray(number) {
+//   var newArray = [];
+
+//   for(var i = 1; i <= number; i++) {
+//     newArray.push(i);
+//   }
+
+//   return newArray;
+// }
+// ```
+// [22. Buggy Code (Part 7)]( https://edabit.com/challenge/CCGBig9eRPFzAHv46 )
+// ```js
+// function swap(a, b) {
+// 	return [b, a]
+// }
+// ```
+// [23. The Farm Problem]( https://edabit.com/challenge/8Qg78sf5SNDEANKti )
+// ```js
+// function animals(chickens, cows, pigs) {
+// 	return chickens*2+cows*4+pigs*4
+// }
+// ```
+// [24. Using the "&&" Operator]( https://edabit.com/challenge/vJCZmgvvDjehyDcDK )
+// ```js
+// function and(a, b) {
+//     if (a && b == true) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+// ```
+
+// [25. Are the Numbers Equal?]( https://edabit.com/challenge/QSnaSH5S3oxZkwcNc )
+// ```js
+// function isSameNum(num1, num2) {
+//     if (num1 === num2) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+// ```
+
+// [26. Football Points]( https://edabit.com/challenge/GwvwXHWCThHZrR7xu )
+// ```js
+// function footballPoints(wins, draws, losses) {
+// 		return (wins * 3) + (draws * 1) + (losses * 0)
+// }
+// ```
+// [27. Convert Hours and Minutes into Seconds]( https://edabit.com/challenge/JesaFi5ntBEbGT8bu )
+// ```js
+// function convert(hours, minutes) {
+//     return (hours * 60 * 60) + (minutes * 60)
+// }
+// ```
+
+// [28. Fix the Expression]( https://edabit.com/challenge/FipbQSYquQLPZ8QXG )
+// ```js
+// function isSeven(x) {
+//     if (x === 7) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+// ```
+
+// [29. Equality Check]( https://edabit.com/challenge/BGvTMfwxYDRbtaTJ3 )
+// ```js
+// function checkEquality(a, b) {
+//     if (a === b) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+// ```
+
+// [30. Profitable Gamble]( https://edabit.com/challenge/ghbHrRnRiDz9fvQNF )
+// ```js
+// function profitableGamble(prob, prize, pay) {
+//     if (prob * prize > pay) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+// ```
+
+// [31. Boolean to String Conversion]( https://edabit.com/challenge/KSTkFSnaYBJdo6PHx )
+// ```js
+// function boolToString(flag) {
+//     if(flag == true){
+//         return "true"
+//     }
+//     else{
+//         return "false"
+//     }
+// }
+// ```
+
+// [32. Using Arrow Functions]( https://edabit.com/challenge/QkvDge63crdGnMfvM )
+// I don’t know
+// [33. Frames Per Second]( https://edabit.com/challenge/d9suvbchE2bnHNQuK )
+// ```js
+// function frames(minutes, fps) {
+//     return minutes * fps * 60
+// }
+// ```
+
+// [34. Miserable Parody of a Calculator]( https://edabit.com/challenge/fh9i7k936rvfjnCYR )
+// I don’t know
+// [35. Buggy Code (Part 4)]( https://edabit.com/challenge/4SmqXvQtL6qRgzfha )
+// ```js
+// function greeting(name) {
+//   let a = "Hello, " + name + "!";
+//   if(name == "Mubashir") {
+//     return "Hello, my Love!";
+//   }
+//     else{
+//         return a
+//     }
+// }
+// ```
+
+// [36. Two Makes Ten]( https://edabit.com/challenge/5erCDJ8eJDrXkmwTK )
+// ```js
+// function makesTen(a, b) {
+//     let f = a + b
+//     if(f == 10 || a == 10 || b == 10){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// ```
+
+// [37. Let's Fuel Up!]( https://edabit.com/challenge/YMWDcSuYwYvve3HZj )
+// ```js
+// function calculateFuel(n) {
+//     let df = n * 10 
+// if(df >= 100){
+//     return df
+// } 
+//     else{
+//         return 100
+//     }
+// }
+// ```
+
+// [38. Buggy Code (Part 2)]( https://edabit.com/challenge/uE9AJ4sSrrpSASMpu )
+// ```js
+// function maxNum(n1,n2) {
+//     if (n1>n2) {
+//       return n1
+//     }
+//     else{
+//     return n2
+//   }
+// }
+// ```
+
+// [39. Pair Management]( https://edabit.com/challenge/BFnsRqe8PFvEwcRNt )
+// ```js
+// function makePair(num1, num2) {
+// 	return [num1, num2]
+	
+// }
+// ```
+// [40. Compare Strings by Count of Characters]( https://edabit.com/challenge/yHGowWucg3k2kJdZ4 )
+// ```js
+// function comp(str1, str2) {
+// 	if (str1.length === str2.length)	{
+// 		return true
+// 	}else{
+// 		return false
+// 	}
+// }
+// ```
+// [41. Is the String Empty?]( https://edabit.com/challenge/EzbfiquDoAc2Zc9FL )
+// ```js
+// function isEmpty(s) {
+// 	if (s.length == 0)	{
+// 		return true
+// 	}else{
+// 		return false
+// 	}
+// }
+// ```
+// [42. Check if an Integer is Divisible By Five]( https://edabit.com/challenge/iBQYbSHZGhpktLRdn )
+// ```js
+// function divisibleByFive(n) {
+// 	return n % 5 == 0
+// }
+// ```
+// [43. Multiple of 100]( https://edabit.com/challenge/qMr6wYGr6NaXAPQGF )
+// ```js
+// function divisible(num) {
+// 	if (1 / num == 1 || 1 / num == -1){
+// 		return false
+// 	}
+// 	else{
+// 		return true
+// 	}
+// }
+// ```
+// [44. Recursion: Length of a String]( https://edabit.com/challenge/4MSbtYFBiRtxHEkY8 )
+// ```js
+// function length(str) {
+// 	return str.length
+// }
+// ```
+// [45. Divides Evenly]( https://edabit.com/challenge/JfB9mWmbwYHbupxCB )
+// I don’t know
+// [46. Return a String as an Integer]( https://edabit.com/challenge/rGsgEswWuW339yNxY )
+// I don’t know
+// [47. Area of a Rectangle]( https://edabit.com/challenge/g6b9HqkXqWu6GpfTo )
+// **I don’t know
+// **[48. Minimal I: If Boolean Then Boolean]( https://edabit.com/challenge/nEdLGbAZQ5LaiumP6 )
+// ```js
+// function isEven(n) {
+//     if(n % 2 === 0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// ```
+	
+// [49. Concatenate First and Last Name into One String](https://edabit.com/challenge/RQwdZmtrW8mCnuCMN)
+// ```js
+// function concatName(firstName, lastName) {
+// 	return lastName+ ", "+ firstName
+// }
+// ```
+// [50. Buggy Code](https://edabit.com/challenge/r2MbSxquAGPnDkjQx)
+// ```js
+// function has_bugs(buggy_code) {
+// 	if (buggy_code == true) {
+// 		return "sad days"
+// 	} else  {
+// 		return "it's a good day"
+// 	}
+// }
+// ```
